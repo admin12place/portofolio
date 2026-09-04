c37a85c54297e1041e178272ea0a2b7dca175ceb
admin12place <thierry@randovelo34.fr>
2026-09-04 08:14:58 +0200

    CSS du menu principal et liens des réseaux sociaux

--- portfolio/template-parts/social-network.php
+++ portfolio/template-parts/social-network.php
@@ -1,6 +1,6 @@
 <section class="network-wrapper">
-    <a class="social-link" href=""><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-facebook.png'?>" title="FaceBook" alt="Lien vers FaceBook"/></a>
-    <a class="social-link" href=""><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-insta.png'?>" title="Instagram" alt="Lien vers Instagram"/></a>
-    <a class="social-link" href=""><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-twitterx.png'?>" title="X (ex Twitter)" alt="Lien vers X (ex Twitter)"/></a>
-    <a class="social-link" href=""><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-youtube.png'?>" title="YouTube" alt="Lien vers YouTube"/></a>
+    <a class="social-link" href="https://fr-fr.facebook.com/" target="_blank"><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-facebook.png'?>" title="FaceBook" alt="Lien vers FaceBook"/></a>
+    <a class="social-link" href="https://www.instagram.com/" target="_blank"><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-insta.png'?>" title="Instagram" alt="Lien vers Instagram"/></a>
+    <a class="social-link" href="https://x.com/" target="_blank"><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-twitterx.png'?>" title="X (ex Twitter)" alt="Lien vers X (ex Twitter)"/></a>
+    <a class="social-link" href="https://www.youtube.com/" target="_blank"><img class="icon-network" src="<?php echo get_stylesheet_directory_uri() . '/assets/icon-youtube.png'?>" title="YouTube" alt="Lien vers YouTube"/></a>
 </section>
